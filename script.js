dade13e6ca1753a2deef3360b7718762e64c682b
kev1n-02 <yalamakevin@gmail.com>
2026-04-04 22:16:01 -0500

    Se agregan nuevas funcionalidades en la branch

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function mostrarMensaje() {
     document.getElementById("mensaje").innerText = "¡Hola! Esta funcionalidad fue creada para la práctica de Git.";
 }
+function mostrarInfo() {
+    document.getElementById("info").innerText = "Esta nueva rama contiene mejoras agregadas al proyecto base.";
+}
